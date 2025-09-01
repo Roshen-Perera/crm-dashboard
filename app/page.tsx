02da38e931972a8abaab0806d4a306fef843d8b7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-01 18:16:57 +0530

    Refactor main layout to enhance structure and styling, including search input integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/sideBar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { Search } from "lucide-react";
 
 
 export default function Home() {
@@ -7,8 +8,20 @@ export default function Home() {
     <>
       <SidebarProvider>
         <AppSidebar />
-        <main>
-          
+        <main className="w-full bg-[#f4f7fd]">
+          <div className="flex items-center justify-between px-6 py-4">
+            <h1 className="text-xl font-semibold text-gray-800">
+              Hello Evano <span className="inline-block">👋</span>,
+            </h1>
+            <div className="relative w-64">
+              <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-full rounded-md border bg-white pl-10 pr-4 py-2 text-sm border-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+          </div>
         </main>
       </SidebarProvider>
     </>
