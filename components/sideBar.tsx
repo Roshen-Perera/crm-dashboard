de0b634c98c2af80710ce963c01f6019a64eec08
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-01 15:00:55 +0530

    Refactor sidebar component layout and styling for improved structure

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -10,6 +10,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import Image from "next/image";
 
 // Menu items.
 const items = [
@@ -48,16 +49,30 @@ const items = [
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent className="bg-white">
+      <SidebarContent className="bg-white border-0">
         <SidebarGroup>
           <SidebarGroupLabel>
-
+            <div className="flex flex-row mt-5 items-center gap-2 text-black">
+              <Image
+                src="/assets/icons/icon.png"
+                alt="Dashboard"
+                width={24}
+                height={24}
+                className="object-contain"
+              />
+              <span className=" font-bold text-2xl leading-none">
+                Dashboard
+              </span>
+            </div>
           </SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="mt-10 ml-2 gap-5">
               {items.map((item) => (
                 <SidebarMenuItem className="text-[#9197B3]" key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton
+                    className="text-base hover:bg-[#5932EA] hover:text-[#ECEAFF] p-5 rounded-md cursor-pointer transition-colors duration-200"
+                    asChild
+                  >
                     <a href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
