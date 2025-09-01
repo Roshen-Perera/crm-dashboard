198a663b49240f56cabb79ed3c87116a37713a96
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-01 23:33:46 +0530

    Refactor statsOverview component for improved layout and icon usage

--- components/statsOverview.tsx
+++ components/statsOverview.tsx
@@ -1,19 +1,19 @@
-import { ArrowUpNarrowWide } from 'lucide-react';
+import { ArrowUp } from 'lucide-react';
 import React from 'react'
 
 const statsOverview = () => {
   return (
     <div className="grid grid-cols-3">
-      <div className="flex">
+      <div className="flex items-center">
         <div className="bg-[#D3FFE7] rounded-full w-12 h-12"></div>
-        <div className='flex flex-col'>
-            <p className='text-xs text-gray-400'>Earning</p>
-            <p className='text-3xl font-semibold'>$198k</p>
-            <div className='flex flex-row'>
-                <ArrowUpNarrowWide/>
-                <p className='text-xs'>37.8%</p>
-                <p className='text-xs'>Since last month</p>
-            </div>
+        <div className="flex flex-col">
+          <p className="text-xs text-gray-400">Earning</p>
+          <p className="text-3xl font-semibold">$198k</p>
+          <div className="flex flex-row">
+            <ArrowUp />
+            <p className="text-xs">37.8%</p>
+            <p className="text-xs">Since last month</p>
+          </div>
         </div>
       </div>
     </div>
