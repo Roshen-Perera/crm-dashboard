fba00729d1a6816b9fead7abc6dbbb673ce54293
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-01 14:30:58 +0530

    Wrap main content with SidebarProvider and include AppSidebar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 
 export default function Home() {
   return (
     <>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+        </main>
+      </SidebarProvider>
     </>
   );
 }
-  
