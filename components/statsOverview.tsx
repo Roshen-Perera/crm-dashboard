fabf067176516210247701afbf0fe3cd9108c43a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-02 01:03:18 +0530

    Refactor statsOverview component to update labels and improve data representation

--- components/statsOverview.tsx
+++ components/statsOverview.tsx
@@ -1,4 +1,4 @@
-import { ArrowUp } from 'lucide-react';
+import { ArrowDown, ArrowUp } from 'lucide-react';
 import React from 'react'
 
 const statsOverview = () => {
@@ -11,21 +11,21 @@ const statsOverview = () => {
             <p className="text-xs text-gray-400">Earning</p>
             <p className="text-3xl font-semibold">$198k</p>
             <div className="flex flex-row items-center gap-1">
-              <ArrowUp className="text-green-500" width={15} height={15} />
-              <p className="text-xs text-green-500">37.8%</p>
-              <p className="text-xs">Since last month</p>
+              <ArrowUp className="text-[#00AC4F]" width={15} height={15} />
+              <p className="text-xs text-[#00AC4F]">37.8%</p>
+              <p className="text-xs">this month</p>
             </div>
           </div>
         </div>
         <div className="flex items-center gap-4">
           <div className="bg-[#CAF1FF] rounded-full w-18 h-18"></div>
           <div className="flex flex-col">
-            <p className="text-xs text-gray-400">Earning</p>
-            <p className="text-3xl font-semibold">$198k</p>
+            <p className="text-xs text-gray-400">Balance</p>
+            <p className="text-3xl font-semibold">$2.4k</p>
             <div className="flex flex-row items-center gap-1">
-              <ArrowUp className="text-green-500" width={15} height={15} />
-              <p className="text-xs text-green-500">37.8%</p>
-              <p className="text-xs">Since last month</p>
+              <ArrowDown className="text-[#D0004B]" width={15} height={15} />
+              <p className="text-xs text-[#D0004B]">2%</p>
+              <p className="text-xs">this month</p>
             </div>
           </div>
         </div>
