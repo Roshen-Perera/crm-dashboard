e41d469160c7f9d988200c28df4d0655381ebfe0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-01 23:35:41 +0530

    Enhance statsOverview component by adding icon color and size for better visibility

--- components/statsOverview.tsx
+++ components/statsOverview.tsx
@@ -9,9 +9,9 @@ const statsOverview = () => {
         <div className="flex flex-col">
           <p className="text-xs text-gray-400">Earning</p>
           <p className="text-3xl font-semibold">$198k</p>
-          <div className="flex flex-row">
-            <ArrowUp />
-            <p className="text-xs">37.8%</p>
+          <div className="flex flex-row items-center">
+            <ArrowUp className="text-green-500" width={15} height={15} />
+            <p className="text-xs text-green-500">37.8%</p>
             <p className="text-xs">Since last month</p>
           </div>
         </div>
