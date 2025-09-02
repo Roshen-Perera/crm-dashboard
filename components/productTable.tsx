6dff4c864c421b9d942fa9c7364f10140ecdf82a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-02 18:24:50 +0530

    fix: update layout and styling of search input and dropdown in productTable component

--- components/productTable.tsx
+++ components/productTable.tsx
@@ -10,26 +10,25 @@ const productTable = () => {
           <div>
             <CardTitle>Product Sell</CardTitle>
           </div>
-          <div className='flex'>
+          <div className="flex gap-4">
             <div className="relative w-48 md:w-64">
               <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
               <input
                 type="text"
                 placeholder="Search"
-                className="w-full rounded-lg border-none bg-white pl-10 pr-4 py-2.5 text-sm
+                className="w-full rounded-lg border-none bg-gray-100 pl-10 pr-4 py-2.5 text-sm
                      focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 focus:bg-white
                      placeholder:text-gray-400 transition-all duration-200"
               />
             </div>
-            <div className="relative w-48 md:w-64">
-              <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
-              <input
-                type="text"
-                placeholder="Search"
-                className="w-full rounded-lg border-none bg-white pl-10 pr-4 py-2.5 text-sm
+            <div className="relative w-48 md:w-40">
+                <select className="w-full rounded-lg border-none bg-gray-100 pl-4 pr-4 py-2.5 text-sm
                      focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 focus:bg-white
-                     placeholder:text-gray-400 transition-all duration-200"
-              />
+                     placeholder:text-gray-400 transition-all duration-200">
+                  <option value="">Last 30 Days</option>
+                  <option value="product1">Product 1</option>
+                  <option value="product2">Product 2</option>
+                </select>
             </div>
           </div>
         </CardHeader>
