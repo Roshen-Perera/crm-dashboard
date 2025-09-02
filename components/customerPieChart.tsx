96e739fef8fcb1faf04a4e1db3bc9e7122039425
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-02 16:37:51 +0530

    fix: update chart data in CustomerPieChart component for accuracy

--- components/customerPieChart.tsx
+++ components/customerPieChart.tsx
@@ -20,11 +20,8 @@ import {
 export const description = "A donut chart with text";
 
 const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 190, fill: "var(--color-other)" },
+  { browser: "chrome", visitors: 35, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 65, fill: "var(--color-safari)" },
 ];
 
 const chartConfig = {
@@ -38,19 +35,7 @@ const chartConfig = {
   safari: {
     label: "Safari",
     color: "var(--chart-2)",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "var(--chart-3)",
-  },
-  edge: {
-    label: "Edge",
-    color: "var(--chart-4)",
-  },
-  other: {
-    label: "Other",
-    color: "var(--chart-5)",
-  },
+  }
 } satisfies ChartConfig;
 
 export function CustomerPieChart() {
