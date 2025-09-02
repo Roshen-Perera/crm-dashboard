b3e13427b8e68dba49fe6d9c4f6def98c3f1b9f9
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-02 13:44:14 +0530

    refactor: simplify layout structure for EarningOverview in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,7 @@ export default function Home() {
         <div className="flex items-center justify-center p-6">
           <StatsOverview />
         </div>
-        <div className="flex p-9">
-          <EarningOverview />
+        <div className="p-9">
           <EarningOverview />
         </div>
       </SidebarInset>
